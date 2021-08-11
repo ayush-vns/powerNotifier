e6a4ce6ef901f188936fad894e2dab19745ab96d
ayush-vns <ayush.mishra.ald@gmail.com>
2021-08-12 00:03:22 +0530

    My first commit
    First commit to powerNotifier

---
+++ Notifier/powerNotifier.py
@@ -0,0 +1,71 @@
+import psutil
+from win10toast import ToastNotifier
+import time
+import schedule
+
+ico_path1 = 'C:/Users/Laptop Point/python_programs/onl/Power Notifier1/battery_caution2.ico'
+ico_path2 = 'C:/Users/Laptop Point/python_programs/onl/Power Notifier1/battery_caution.ico'
+
+def notify_start():
+    toast = ToastNotifier()
+    toast.show_toast('Batter Alert', 
+                     'Power notifier is running...',
+                     duration=5, 
+                     icon_path=ico_path1)
+
+def notify_error():
+    toast = ToastNotifier()
+    toast.show_toast('Battery Notifier Error', 
+                     'Unexpected Error Ocurred: Closing Power Notifier...', 
+                     duration=5, 
+                     icon_path=ico_path1)
+    
+def power_info():
+    battery = psutil.sensors_battery()
+    plugged_in = battery.power_plugged
+    return plugged_in, battery.percent
+
+def notify_fullcharge():
+    toast = ToastNotifier()
+    toast.show_toast('Battery Alert (>97%)',
+                     'Battery is fully charged: {}%.'.format(battery_percent),
+                     duration=5,
+                     icon_path=ico_path1)
+
+def notify_batterylow():
+    toast = ToastNotifier()
+    toast.show_toast('Battery Alert (<15%)',
+                     'Battery is about to be discharged: {}%.'.format(battery_percent),
+                     duration=5,
+                     icon_path=ico_path2)
+    
+notify_start()
+    
+try:
+    def Battery_Alert():    
+        
+        plugged_in, battery_percent = power_info()
+
+        # print(plugged_in)
+
+        if plugged_in == True:
+            print('Power Plugged_in = True\nBattery Percentage: {}%'.format(battery_percent))
+            if battery_percent >= 97:
+                notify_fullcharge()
+
+        else:
+            _, battery_percent = power_info()
+            print('Power plugged_in = False\nBattery Percentage: {}%'.format(battery_percent))
+            if battery_percent <= 15:
+                notify_batterylow()
+                
+                
+    schedule.every(59).seconds.do(Battery_Alert)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+        
+except:
+    print('Unexpected Error Occured: Closing Batter Notifier...')
+    notify_error()
---
+++ README.md
@@ -0,0 +1,11 @@
+# Power Notifier
+ 
+    Power Notifier notifies you about when your PC battery has reached below certain percentage or have been fully charged or about to be discharged.
+    
+## How to use this on your PC
+
+    Since this project is still in development but I have released some basis codes of power notifier. You can use power notifier by just putting `powerNotifier.py` file in your PCs startup folder. This has been tested only for windows. But if you know what you are doing you can use it however you want. This is no current license for this basic code version.
+    
+### Startup folder directory for windows
+
+dir = `C:\Users\user_name\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup'
